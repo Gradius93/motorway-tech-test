1cfef48fc87af3fb8fa5eaf8acbe50d40a312b4d
Sean Gray <sean.myles.gray@gmail.com>
2021-09-27 16:24:45 +0100

    basic express app created

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
