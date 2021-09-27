8b6c7f98ae06e2fb8e037f9745461939bd5b28e6
Sean Gray <sean.myles.gray@gmail.com>
2021-09-27 16:31:05 +0100

    added DB and initial index route

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const path = require('path')
 
+const Sequelize = require('sequelize')
+
+const db = new Sequelize('vehiclesDB', 'postgres', 'S34n', {
+    host: 'localhost',
+    dialect: 'postgres' 
+  });
+
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
+
+
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('INDEX'))
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`))
